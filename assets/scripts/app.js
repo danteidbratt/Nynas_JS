1f5c883864d814ad1b9147b6188afc578284ab27
danteidbratt <danteidbratt@users.noreply.github.com>
2018-02-26 15:55:53 +0100

    ok

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,59 +2,58 @@ function Table(table) {
     this.container = document.querySelector(table.container);
     this.head = table.head;
     this.rows = table.rows;
-
-    function printHead(table) {
-        var head = document.createElement('thead');
-        head.style.backgroundColor = 'purple';
-        head.style.color = 'white';
-        var tr = document.createElement('tr');
-        for(let value in table.head) {
-            let th = document.createElement('th');
-            th.innerHTML = table.head[value];
-            tr.appendChild(th);
-        }
-        head.appendChild(tr);
-        table.container.appendChild(head);
-    };
-
-    function printBody(table) {
-        var tbody = document.createElement('tbody');
-        table.rows.forEach((row) => {
-            var tr = document.createElement('tr');
-            for(let value in row) {
-                let td = document.createElement('td');
-                td.innerHTML = row[value];
-                tr.appendChild(td);
-            }
-            tbody.appendChild(tr);
-        });
-        table.container.appendChild(tbody);
-    };
-    printHead(this);
-    printBody(this);
 };
+
+Table.prototype.printHead = function(table) {
+    var head = document.createElement('thead');
+    head.style.backgroundColor = 'purple';
+    head.style.color = 'white';
+    var tr = document.createElement('tr');
+    for (let value in table.head) {
+        let th = document.createElement('th');
+        th.innerHTML = table.head[value];
+        tr.appendChild(th);
+    }
+    head.appendChild(tr);
+    table.container.appendChild(head);
+}
+
+Table.prototype.printBody = function(table) {
+    var tbody = document.createElement('tbody');
+    table.rows.forEach((row) => {
+        var tr = document.createElement('tr');
+        for (let value in row) {
+            let td = document.createElement('td');
+            td.innerHTML = row[value];
+            tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    });
+    table.container.appendChild(tbody);
+}
 
 function TrainTable(trainTable) {
     Table.call(this, {
         container: '#train-table',
-        head: {a: 'Nr', b: 'Avgång', c: 'Ankomst'},
+        head: {
+            a: 'Nr',
+            b: 'Avgång',
+            c: 'Ankomst'
+        },
         rows: trainTable.rows
     });
-
     this.status = trainTable.status;
-    
-    function printCaption() {
-        var container = document.querySelector('#train-table');
-        var caption = document.createElement('caption');
-        container.innerHTML = '<caption class="ml-3">' + this.status + '</caption> ' + container.innerHTML;
-    }
-    printCaption();
 };
 
 function WeatherTable(info) {
     Table.call(this, {
         container: '#weather-table',
-        head: {a: 'Klocka', b: 'Väder', c: 'Värme', d: 'Vind'},
+        head: {
+            a: 'Klocka',
+            b: 'Väder',
+            c: 'Värme',
+            d: 'Vind'
+        },
         rows: info
     });
 };
@@ -66,6 +65,12 @@ function pointPrototype(subClass, superClass) {
 
 pointPrototype(TrainTable, Table);
 pointPrototype(WeatherTable, Table);
+
+TrainTable.prototype.printCaption = function() {
+    var container = document.querySelector('#train-table');
+    var caption = document.createElement('caption');
+    container.innerHTML = '<caption class="ml-3">' + this.status + '</caption> ' + container.innerHTML;
+}
 
 function TrafficRow(nr, departs, arrives) {
     this.nr = nr;
@@ -80,36 +85,45 @@ function WeatherRow(time, weather, temperature, wind) {
     this.wind = wind;
 };
 
-var trafficInfo = [
-   {location: 'Stockholm',
-    rows: [new TrafficRow('42', '11:30', '12:15'),
-           new TrafficRow('42', '12:30', '13:15'),
-           new TrafficRow('42', '13:30', '14:15'),
-           new TrafficRow('42', '14:30', '15:15'),
-           new TrafficRow('42', '15:30', '16:15')],
-    status: 'Inga problem i trafiken'
-}, 
-   {location: 'Norrköping',
-    rows: [new TrafficRow('36', '09:10', '11:30'),
-           new TrafficRow('36', '11:10', '13:30'),
-           new TrafficRow('36', '13:10', '15:30')],
-    status: 'Förseningar p.g.a. vagnfel'
-}, 
-   {location: 'Södertälje',
-    rows: [new TrafficRow('21', '11:00', '11:20'),
-           new TrafficRow('21', '12:00', '13:20'),
-           new TrafficRow('21', '13:00', '14:20'),
-           new TrafficRow('21', '14:00', '15:20')],
-    status: 'Inga problem i trafiken'
-}];
+var trafficInfo = [{
+        location: 'Stockholm',
+        rows: [new TrafficRow('42', '11:30', '12:15'),
+            new TrafficRow('42', '12:30', '13:15'),
+            new TrafficRow('42', '13:30', '14:15'),
+            new TrafficRow('42', '14:30', '15:15'),
+            new TrafficRow('42', '15:30', '16:15')
+        ],
+        status: 'Inga problem i trafiken'
+    },
+    {
+        location: 'Norrköping',
+        rows: [new TrafficRow('36', '09:10', '11:30'),
+            new TrafficRow('36', '11:10', '13:30'),
+            new TrafficRow('36', '13:10', '15:30')
+        ],
+        status: 'Förseningar p.g.a. vagnfel'
+    },
+    {
+        location: 'Södertälje',
+        rows: [new TrafficRow('21', '11:00', '11:20'),
+            new TrafficRow('21', '12:00', '13:20'),
+            new TrafficRow('21', '13:00', '14:20'),
+            new TrafficRow('21', '14:00', '15:20')
+        ],
+        status: 'Inga problem i trafiken'
+    }
+];
 
 var weatherInfo = [new WeatherRow('9:00', 'Moln', '16°C', '3m/s'),
     new WeatherRow('12:00', 'Sol', '18°C', '4m/s'),
     new WeatherRow('15:00', 'Sol', '20°C', '2m/s'),
     new WeatherRow('18:00', 'Regn', '19°C', '3m/s'),
-    new WeatherRow('21:00', 'Moln', '17°C', '1m/s')];
+    new WeatherRow('21:00', 'Moln', '17°C', '1m/s')
+];
 
 var wt = new WeatherTable(weatherInfo);
+wt.printHead(wt);
+wt.printBody(wt);
 
 var button = document.querySelector('#train-button');
 button.addEventListener('click', (event) => {
@@ -122,9 +136,11 @@ button.addEventListener('click', (event) => {
     var p = document.querySelector('#train-table ~ p');;
     if (specificInfo.length > 0) {
         var tt = new TrainTable(specificInfo[0]);
+        tt.printHead(tt);
+        tt.printBody(tt);
+        tt.printCaption();
         p.innerHTML = 'Åker från: ' + specificInfo[0].location;
-    }
-    else {
+    } else {
         p.innerHTML = '';
     }
     event.preventDefault();
